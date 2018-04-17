a3e28e6cfe205cb514f004c0e6ce1b7bc25bf4dd
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-04-17 16:55:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var recipes = {};
 
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {key : value});
+ var newObject = Object.assign({}, object, {key : value});
+ return newObject;
 }
